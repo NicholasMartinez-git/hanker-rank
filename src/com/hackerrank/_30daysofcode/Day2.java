650b28bf9bb73423ca2a4d937d87d3508044f2c1
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-09-15 20:38:25 -0500

    feat: Complete Day2

---
+++ src/com/hackerrank/_30daysofcode/Day2.java
@@ -0,0 +1,33 @@
+/* Day 2: Operators */
+
+package com.hackerrank._30daysofcode;
+
+import java.util.Scanner;
+
+public class Day2 {
+    public static void main(String[] args) {
+        // TODO
+        // Given the meal price (base cost of a meal), tip percent (the percentage of the meal price being added as tip),
+        // and tax percent (the percentage of the meal price being added as tax) for a meal,
+        // find and print the meal's total cost Round the result to the nearest integer.
+
+        // Create a Scanner object to read input from stdin.
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("What is the cost of the meal?");
+        double meal_cost = scan.nextDouble();
+
+        System.out.println("What is the tip percent?");
+        int tip_percent = scan.nextInt();
+
+        System.out.println("What is the tax percent?");
+        int tax_percent = scan.nextInt();
+        scan.nextLine(); // handle next line character (enter key)
+
+        double tip = ((double) tip_percent / 100) * meal_cost;
+        double tax = ((double) tax_percent / 100) * meal_cost;;
+        double total = meal_cost + tip + tax;
+
+        System.out.println("The meal's total cost is: $" + Math.round(total));
+    }
+}
