39e80b31d22bf860545d65c17a17b090751f2616
Nicholas Martinez <78042630+NicholasMartinez-git@users.noreply.github.com>
2021-09-17 11:35:39 -0500

    feat: Create day4

---
+++ src/com/hackerrank/_30daysofcode/day4/Main.java
@@ -0,0 +1,10 @@
+package com.hackerrank._30daysofcode.day4;
+
+public class Main {
+    public static void main(String[] args) {
+        Person person1 = new Person(-1);
+        Person person2 = new Person(10);
+        Person person3 = new Person(16);
+        Person person4 = new Person(18);
+    }
+}
